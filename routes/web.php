ab5ce4d762bfa89b1bf375bbabcdd8d390693d4d
engineer_tacky <tsubasa801@gmail.com>
2022-08-05 23:02:12 +0900

    sample

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Sample\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sample', [IndexController::class, 'show']);
+Route::get('/sample/{id}', [IndexController::class, 'showId']);
