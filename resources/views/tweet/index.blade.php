b3cb944f3e569fa1b3be52b9dc5b4db5cd12a72e
engineer_tacky <tsubasa801@gmail.com>
2022-08-05 23:54:52 +0900

    つぶやきを投稿

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <title>つぶやきアプリ</title>
 </head>
+
 <body>
     <h1>つぶやきアプリ</h1>
     <div>
-        @foreach ($tweets as $tweet)
-            <p>{{ $tweet->content }}</p>
-        @endforeach
+        <p>投稿フォーム</p>
+        <form action="{{ route('tweet.create') }}" method="post">
+            @csrf
+            <label for="tweet-content">つぶやき</label>
+            <span>140文字まで</span>
+            <textarea name="tweet" id="tweet-content" type="text" placeholder="つぶやきを入力"></textarea>
+            <button type="submit">投稿</button>
+            @error('tweet')
+                <p style="color: red;">{{ $message }}</p>
+            @enderror
+
+            @foreach ($tweets as $tweet)
+                <p>{{ $tweet->content }}</p>
+            @endforeach
+        </form>
     </div>
 </body>
+
 </html>
