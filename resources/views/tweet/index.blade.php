f8adacb815cb82676330c28a97677860f653d891
engineer_tacky <tsubasa801@gmail.com>
2022-08-06 11:00:26 +0900

    つぶやきの削除

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -10,6 +10,9 @@
     <h1>つぶやきアプリ</h1>
     <div>
         <p>投稿フォーム</p>
+        @if (session('feedback.success'))
+            <p style="color: green;">{{ session('feedback.success') }}</p>
+        @endif
         <form action="{{ route('tweet.create') }}" method="post">
             @csrf
             <label for="tweet-content">つぶやき</label>
@@ -19,16 +22,21 @@
             @error('tweet')
                 <p style="color: red;">{{ $message }}</p>
             @enderror
-
-            @foreach ($tweets as $tweet)
-                <details>
-                    <summary>{{ $tweet->content }}</summary>
-                    <div>
-                        <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-                    </div>
-                </details>
-            @endforeach
         </form>
+
+        @foreach ($tweets as $tweet)
+            <details>
+                <summary>{{ $tweet->content }}</summary>
+                <div>
+                    <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                </div>
+                <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                    @method('delete')
+                    @csrf
+                    <button type="submit">削除</button>
+                </form>
+            </details>
+        @endforeach
     </div>
 </body>
 
